3d1ac7970de4aee6784f7fc2f8eb0d9f4393dd4b
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-05-03 14:05:05 +0900

    add log settings

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 3000;
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/api/hello", async (req, res) => {
   res.json({
