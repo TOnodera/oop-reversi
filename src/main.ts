1452b4eb671582588014a207ec3b38c2ac1ae1e3
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-05-03 15:29:16 +0900

    error handler implemented.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
 import express from "express";
 import morgan from "morgan";
+import "express-async-errors";
 
 const PORT = 3000;
 const app = express();
-
 app.use(morgan("dev"));
+
+app.get("/api/error", async (req, res) => {
+  throw new Error("Error endpoint");
+});
 
 app.get("/api/hello", async (req, res) => {
   res.json({
     message: "Hello, world",
   });
 });
+
+const errorHandler = (
+  error: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) => {
+  console.error("unexpected error occurred", error);
+  res.status(500).json({
+    message: "Unexpected error occurred",
+  });
+};
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Reversi application started: http://localhost:${PORT}`);
