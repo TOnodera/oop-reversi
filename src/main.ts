da5bf8f46f5704bf1236210490f585486b3d9460
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-05-04 23:37:10 +0900

    プレゼンテーション層作成

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,9 @@
 import express, { json } from "express";
 import morgan from "morgan";
 import "express-async-errors";
-import mysql from "mysql2/promise";
-import { GameGateway } from "./dataaccess/gameGateway";
-import { TurnGateway } from "./dataaccess/turnGateway";
-import { MoveGateway } from "./dataaccess/moveGateway";
-import { SquareGateway } from "./dataaccess/squareGateway";
-
-const PORT = 3000;
-const EMPTY = 0;
-const DARK = 1;
-const LIGHT = 2;
-const INITIAL_BOARD = [
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-];
-const gameGateway = new GameGateway();
-const turnGateway = new TurnGateway();
-const moveGateway = new MoveGateway();
-const squareGateway = new SquareGateway();
+import { PORT } from "./application/constants";
+import { gameRouter } from "./presentation/gameRouter";
+import { turnRouter } from "./presentation/turnRouter";
 
 const app = express();
 
@@ -34,131 +13,6 @@ app.use(json());
 
 app.get("/api/error", async (req, res) => {
   throw new Error("Error endpoint");
-});
-
-app.post("/api/games", async (req, res) => {
-  const now = new Date();
-  const conn = await connectMySql();
-  try {
-    await conn.beginTransaction();
-    const gameRecord = await gameGateway.insert(conn, now);
-
-    const turnRecord = await turnGateway.insert(
-      conn,
-      gameRecord.id,
-      0,
-      DARK,
-      now
-    );
-
-    await squareGateway.insertAll(conn, turnRecord.id, INITIAL_BOARD);
-
-    await conn.commit();
-  } catch (error) {
-    console.error(error);
-  } finally {
-    await conn.end();
-  }
-  res.status(200).end();
-});
-
-app.get("/api/games/latest/turns/:turnCount", async (req, res) => {
-  const turnCount = parseInt(req.params.turnCount);
-  const conn = await connectMySql();
-  try {
-    const gameRecord = await gameGateway.findLatest(conn);
-    if (!gameRecord) {
-      throw new Error("Latest game not found");
-    }
-
-    const turnRecord = await turnGateway.findForGameIdAndTurnCount(
-      conn,
-      gameRecord.id,
-      turnCount
-    );
-    if (!turnRecord) {
-      throw new Error("specified turn not found");
-    }
-
-    const squareRecords = await squareGateway.findForTurnId(
-      conn,
-      turnRecord.id
-    );
-
-    const board = Array.from(Array(8)).map(() => Array.from(Array(8)));
-    squareRecords.forEach((s) => {
-      board[s.y][s.x] = s.disc;
-    });
-    const responceBody = {
-      turnCount,
-      board,
-      nextDisc: turnRecord.nextDisc,
-      // TODO
-      winnerDisc: null,
-    };
-    res.json(responceBody);
-  } finally {
-    await conn.end();
-  }
-});
-
-app.post("/api/games/latest/turns", async (req, res) => {
-  const turnCount = parseInt(req.body.turnCount);
-  const disc = parseInt(req.body.move.disc);
-  const x = parseInt(req.body.move.x);
-  const y = parseInt(req.body.move.y);
-
-  const conn = await connectMySql();
-  try {
-    // ひとつ前のターンを取得する
-    const gameRecord = await gameGateway.findLatest(conn);
-    if (!gameRecord) {
-      throw new Error("Latest game not found");
-    }
-
-    const previousTurnCount = turnCount - 1;
-    const previousTurnRecord = await turnGateway.findForGameIdAndTurnCount(
-      conn,
-      gameRecord.id,
-      previousTurnCount
-    );
-    if (!previousTurnRecord) {
-      throw new Error("specified turn not found");
-    }
-
-    const squareRecords = await squareGateway.findForTurnId(
-      conn,
-      previousTurnRecord.id
-    );
-
-    const board = Array.from(Array(8)).map(() => Array.from(Array(8)));
-    squareRecords.forEach((s) => {
-      board[s.y][s.x] = s.disc;
-    });
-    // 盤面におけるかチェックする
-    // 石を置く
-    board[y][x] = disc;
-    // ひっくり返す
-    // ターンを保存する
-    const now = new Date();
-    const nextDisc = disc === DARK ? LIGHT : DARK;
-    const turnRecord = await turnGateway.insert(
-      conn,
-      gameRecord.id,
-      turnCount,
-      nextDisc,
-      now
-    );
-
-    await squareGateway.insertAll(conn, turnRecord.id, board);
-
-    await moveGateway.insert(conn, turnRecord.id, disc, x, y);
-
-    await conn.commit();
-  } finally {
-    await conn.end();
-  }
-  res.status(201).end();
 });
 
 app.get("/api/hello", async (req, res) => {
@@ -178,17 +32,11 @@ const errorHandler = (
     message: "Unexpected error occurred",
   });
 };
+
+app.use(gameRouter);
+app.use(turnRouter);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Reversi application started: http://localhost:${PORT}`);
 });
-
-async function connectMySql() {
-  return await mysql.createConnection({
-    host: "mysql",
-    database: "reversi",
-    user: "reversi",
-    password: "password",
-  });
-}
