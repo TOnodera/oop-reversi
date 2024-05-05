5e3b96c4f538664190ce96899b4f3927dbb2f321
t.onodera <t.onodera.1.2.1.5@gmail.com>
2024-05-05 21:39:37 +0900

    ひっくり返す処理の実装の前

--- src/domain/turn/board.ts
+++ src/domain/turn/board.ts
@@ -1,17 +1,33 @@
 import { Disc } from "../turn/disc";
 import { Move } from "../turn/move";
+import { Point } from "./point";
 
 export class Board {
   constructor(private _discs: Disc[][]) {}
 
   place(move: Move): Board {
     // TODO 盤面におけるかチェック
+    // 空出ない場合は石を置くことはできない
+    if (this._discs[move.point.y][move.point.x] !== Disc.Empty) {
+      throw new Error("Selected point is not Empty");
+    }
+
+    // ひっくり返せる点がない場合は置くことはできない
+    const flipPoints = this.listFlipPoints();
+    if (flipPoints.length === 0) {
+      throw new Error("Flip point is emptys");
+    }
+
     // 盤面をコピー
     const newDiscs = this._discs.map((line) => line.map((disc) => disc));
     // 石を置く
     newDiscs[move.point.y][move.point.x] = move.disc;
     // TODO ひっくり返す
     return new Board(newDiscs);
+  }
+
+  private listFlipPoints() {
+    return [new Point(0, 0)];
   }
 
   get discs() {
